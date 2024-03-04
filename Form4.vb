e3a5f2a64439c0a9aab09f160bead879520ed5bd
OgaoNeema <159877009+OgaoNeema@users.noreply.github.com>
2024-03-05 00:04:15 +0300

    commit (#4)
    Co-authored-by: OgaoNeema <neemaogao12@gmail.com>

--- Form4.vb
+++ Form4.vb
@@ -1,3 +1,58 @@
 ﻿Public Class Form4
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Get the current time
+        Dim currentTime As DateTime = DateTime.Now
 
+        ' Determine the appropriate greeting based on the time of day
+        Dim greeting As String
+        If currentTime.Hour < 12 Then
+            greeting = "Good morning"
+        ElseIf currentTime.Hour < 18 Then
+            greeting = "Good afternoon"
+        Else
+            greeting = "Good evening"
+        End If
+
+        ' Update the label text
+        lblGreeting.Text = greeting
+    End Sub
+
+    Private Sub btnWallet_Click(sender As Object, e As EventArgs) Handles btnWallet.Click
+        Me.Close()
+        Dim form3Instance As New Form3()
+        form3Instance.Show()
+    End Sub
+    Private Sub btnHome_Click(sender As Object, e As EventArgs) Handles btnHome.Click
+        Me.Close()
+        Dim form4Instance As New Form4()
+        form4Instance.Show()
+    End Sub
+
+    Private Sub btnBudget_Click(sender As Object, e As EventArgs) Handles btnBudget.Click
+        Me.Close()
+        Dim form5Instance As New Form5()
+        form5Instance.Show()
+    End Sub
+
+    Private Sub btnReports_Click(sender As Object, e As EventArgs) Handles btnReports.Click
+        Me.Close()
+        Dim form6Instance As New Form6()
+        form6Instance.Show()
+    End Sub
+
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        Me.Close()
+        Dim form7Instance As New Form7()
+        form7Instance.Show()
+    End Sub
+
+    Private Sub btnTransaction_Click(sender As Object, e As EventArgs) Handles btnTransaction.Click
+        Me.Close()
+        Dim form8Instance As New Form8()
+        form8Instance.Show()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
 End Class
---
+++ Form8.vb
@@ -0,0 +1,50 @@
+﻿Imports System.IO
+Imports System.Numerics
+Imports System.Runtime.InteropServices
+Imports System.Security.Cryptography.Xml
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox
+
+Public Class Form8
+    Private selectedBudget As String = Nothing
+    Private selectedCategory As String = Nothing
+    Private enteredAmount As Decimal = 0
+
+    Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        comboBoxBudget.Items.AddRange({"Budget 1", "Budget 2", "Budget 3", "Budget 4", "Budget 5"})
+        comboBoxBudget.SelectedIndex = 0
+
+        comboBoxCategory.Items.AddRange({"Food & Drinks", "Bill", "Transport", "House Rent", "Fix & Maintenance", "Utilities", "Groceries", "Entertainment", "Fashion", "Travel", "Beauty Care", "Party", "Gift", "Donation", "Doctor", "Sport", "Insurance", "Gym Membership", "Fee & charge", "Investments", "Kids", "Other"})
+        comboBoxCategory.SelectedIndex = 0
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Get the selected budget
+        selectedBudget = comboBoxBudget.SelectedItem.ToString()
+
+        ' Get the selected category
+        selectedCategory = comboBoxCategory.SelectedItem.ToString()
+
+        ' Get the entered amount
+        If Decimal.TryParse(txtAmount.Text, enteredAmount) Then
+            ' Valid amount entered
+            ' Do something with the selected budget, selected category, and entered amount
+            MessageBox.Show("Selected Budget: " & selectedBudget & vbCrLf &
+                            "Selected Category: " & selectedCategory & vbCrLf &
+                            "Amount: " & enteredAmount.ToString("C"))
+            ' You can save the data to a database or perform further processing here
+        Else
+            ' Invalid amount entered
+            MessageBox.Show("Please enter a valid amount.")
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnHome_Click(sender As Object, e As EventArgs) Handles btnHome.Click
+        Me.Close()
+        Dim form4Instance As New Form4()
+        form4Instance.Show()
+    End Sub
+End Class
