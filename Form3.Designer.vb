7226714014e2ac5571d798073257decf03cbc7e6
Maureen Nyaga <mukamiganya@gmail.com>
2024-03-05 07:13:17 +0300

    Form3 commit

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -22,9 +22,108 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form3"
+        lblName = New Label()
+        txtName = New TextBox()
+        lblCurrency = New Label()
+        ComboBoxCurrency = New ComboBox()
+        lblAmount = New Label()
+        txtAmount = New TextBox()
+        btnSave = New Button()
+        bthHome = New Button()
+        SuspendLayout()
+        ' 
+        ' lblName
+        ' 
+        lblName.AutoSize = True
+        lblName.Location = New Point(28, 52)
+        lblName.Name = "lblName"
+        lblName.Size = New Size(39, 15)
+        lblName.TabIndex = 0
+        lblName.Text = "Name"
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(95, 44)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(187, 23)
+        txtName.TabIndex = 1
+        ' 
+        ' lblCurrency
+        ' 
+        lblCurrency.AutoSize = True
+        lblCurrency.Location = New Point(26, 96)
+        lblCurrency.Name = "lblCurrency"
+        lblCurrency.Size = New Size(55, 15)
+        lblCurrency.TabIndex = 2
+        lblCurrency.Text = "Currency"
+        ' 
+        ' ComboBoxCurrency
+        ' 
+        ComboBoxCurrency.FormattingEnabled = True
+        ComboBoxCurrency.Location = New Point(95, 88)
+        ComboBoxCurrency.Name = "ComboBoxCurrency"
+        ComboBoxCurrency.Size = New Size(187, 23)
+        ComboBoxCurrency.TabIndex = 3
+        ' 
+        ' lblAmount
+        ' 
+        lblAmount.AutoSize = True
+        lblAmount.Location = New Point(31, 143)
+        lblAmount.Name = "lblAmount"
+        lblAmount.Size = New Size(51, 15)
+        lblAmount.TabIndex = 4
+        lblAmount.Text = "Amount"
+        ' 
+        ' txtAmount
+        ' 
+        txtAmount.Location = New Point(95, 135)
+        txtAmount.Name = "txtAmount"
+        txtAmount.Size = New Size(187, 23)
+        txtAmount.TabIndex = 5
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(95, 289)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(75, 23)
+        btnSave.TabIndex = 6
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' bthHome
+        ' 
+        bthHome.Location = New Point(567, 279)
+        bthHome.Name = "bthHome"
+        bthHome.Size = New Size(75, 23)
+        bthHome.TabIndex = 7
+        bthHome.Text = "Home"
+        bthHome.UseVisualStyleBackColor = True
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(bthHome)
+        Controls.Add(btnSave)
+        Controls.Add(txtAmount)
+        Controls.Add(lblAmount)
+        Controls.Add(ComboBoxCurrency)
+        Controls.Add(lblCurrency)
+        Controls.Add(txtName)
+        Controls.Add(lblName)
+        Name = "Form3"
+        Text = "Wallet"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblCurrency As Label
+    Friend WithEvents ComboBoxCurrency As ComboBox
+    Friend WithEvents lblAmount As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents bthHome As Button
 End Class
--- Form3.vb
+++ Form3.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form3
+    Private enteredName As String = Nothing
+    Private selectedCurrency As String = Nothing
+    Private enteredAmount As Decimal = 0
 
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Populate ComboBox with currency options
+        ComboBoxCurrency.Items.AddRange({"USD", "CAD", "EUR", "AED", "AUD", "CNY", "CZK", "DJF", "EGP", "GBP", "HKD", "KES", "JPY", "SOS", "SEK", "TZC", "UGX", "UYU", "UZS", "VND", "XAF", "ZAR"})
+        ' Set default selected item if needed
+        ComboBoxCurrency.SelectedIndex = 0
+    End Sub
+    Private Sub ButtonSubmit_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Get the entered name
+        enteredName = txtName.Text
+
+        ' Get the selected currency
+        selectedCurrency = ComboBoxCurrency.SelectedItem.ToString()
+
+        ' Get the entered amount
+        If Decimal.TryParse(txtAmount.Text, enteredAmount) Then
+            ' Valid amount entered
+            ' Do something with the entered name, selected currency, and amount
+            MessageBox.Show("Name: " & enteredName & vbCrLf &
+                            "Currency: " & selectedCurrency & vbCrLf &
+                            "Amount: " & enteredAmount.ToString("C"))
+            ' You can save the data to a database or perform further processing here
+        Else
+            ' Invalid amount entered
+            MessageBox.Show("Please enter a valid amount.")
+        End If
+    End Sub
+
+    Private Sub bthHome_Click(sender As Object, e As EventArgs) Handles bthHome.Click
+        ' Close the current form (Form3) and show the home form (Form1)
+        Form4.Show()
+        Me.Close()
+    End Sub
 End Class
+
